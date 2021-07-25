df9ed2d82d9d9bd3aa47f8eaf7708dc27c06e9d4
Fillipe <fillipe484@gmail.com>
2021-07-25 13:12:37 -0300

    feat: studies

---
+++ 5-POO/10-methodOverriding/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Animal();
+
+        Dog dog = new Dog();
+
+        dog.speak();
+        animal.speak();
+    }
+}
---
+++ 5-POO/12-abstraction/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Vehicle vehicle = new Vehicle();
+        Car car = new Car();
+
+        car.go();
+
+
+    }
+}
